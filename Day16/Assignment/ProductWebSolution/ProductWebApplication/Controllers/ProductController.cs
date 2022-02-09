1d82fca0c9d521401a51fe4b875256fa19e26009
Jo <jtl.sitompul@gmail.com>
2022-02-09 17:00:29 +0800

    Day-16 Submissions

---
+++ Day16/Assignment/ProductWebSolution/ProductWebApplication/Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> Products = new List<Product>()
+        {
+            new Product()
+            {
+                ProdId = 1,
+                Name = "Hammer",
+                Price = 9.90,
+                SuppId = 101,
+                Qty = 299,
+                Remarks = "Red"
+            },
+            new Product()
+            {
+                ProdId = 2,
+                Name = "Screwdriver",
+                Price = 6.90,
+                SuppId = 101,
+                Qty = 209,
+                Remarks = "Black"
+            },
+            new Product()
+            {
+                ProdId = 3,
+                Name = "Nail",
+                Price = 0.06,
+                SuppId = 101,
+                Qty = 1089,
+                Remarks = "2-inch"
+            },
+            new Product()
+            {
+                ProdId = 4,
+                Name = "Screw",
+                Price = 0.08,
+                SuppId = 101,
+                Qty = 1209,
+                Remarks = "1-inch"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(Products);
+        }
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insert(IFormCollection keyValues)
+        {
+            Product prod = new Product();
+            prod.ProdId = Convert.ToInt32(keyValues["pid"]);
+            prod.Name = keyValues["name"].ToString();
+            prod.Price = Convert.ToDouble(keyValues["price"]);
+            prod.Qty = Convert.ToInt32(keyValues["qty"]);
+            prod.SuppId = Convert.ToInt32(keyValues["sid"]);
+            prod.Remarks = keyValues["remarks"].ToString();
+            Products.Add(prod);
+            //return Content($"Hello {prod.Name}");
+            return RedirectToAction("Index");
+        }
+        //public IActionResult Insert(Product prod)
+        //{
+        //    Products.Add(prod);
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
---
+++ Day16/Assignment/ProductWebSolution/ProductWebApplication/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebApplication.Models
+{
+    public class Product
+    {
+        public int ProdId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int SuppId { get; set; }
+        public int Qty { get; set; }
+        public string Remarks { get; set; }
+    }
+}
